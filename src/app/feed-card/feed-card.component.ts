7f55d616870bef9a3bbdddfdbff6cb59fe676fa6
julienSjv <julien.stjeanvidal@gmail.com>
2018-01-23 13:57:44 +0100

    [fix] global modal

--- src/app/feed-card/feed-card.component.ts
+++ src/app/feed-card/feed-card.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit, Input } from '@angular/core';
-import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
+import { ModalModule } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-feed-card',
   templateUrl: './feed-card.component.html',
   styleUrls: ['./feed-card.component.css'],
-  providers: [NgbModal]
+  providers: [ModalModule]
 
 })
 export class FeedCardComponent implements OnInit {
