b40b31d18f6967670c35779c1c9942e64dc51628
JulienSjv <julien.stjeanvidal@gmail.com>
2018-04-25 00:15:51 +0200

    class="bold"

--- src/app/meteo/meteo.component.ts
+++ src/app/meteo/meteo.component.ts
@@ -24,7 +24,7 @@ export class MeteoComponent implements OnInit, AfterViewInit, OnDestroy {
   city: boolean = false;
   error: boolean = false;
   forecast: boolean = false;
-  meteodetails: boolean = false;
+  meteoDetails: boolean = false;
   country;
   timeZone;
   showMap = false;
@@ -129,7 +129,7 @@ export class MeteoComponent implements OnInit, AfterViewInit, OnDestroy {
     }
   }
 
-  MeteoDetailsToggle() {
-    this.meteodetails = !this.meteodetails;
+  meteoDetailsToggle() {
+    this.meteoDetails = !this.meteoDetails;
   }
 }
