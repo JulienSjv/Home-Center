cc5a5fb9c247915f3a6545847ca99a53e3de996a
JulienSjv <julien.stjeanvidal@gmail.com>
2018-04-26 00:05:41 +0200

    [upd]meteo forecast

--- src/app/meteo/meteo.component.ts
+++ src/app/meteo/meteo.component.ts
@@ -5,6 +5,7 @@ import { TimeZoneService } from '../time-zone.service';
 import { DomSanitizer } from '@angular/platform-browser';
 import { Pays } from '../util/pays';
 import { environment } from '../../environments/environment';
+import { getTime } from 'ngx-bootstrap/chronos/utils/date-getters';
 
 
 @Component({
@@ -16,6 +17,8 @@ import { environment } from '../../environments/environment';
 export class MeteoComponent implements OnInit, AfterViewInit, OnDestroy {
 
   readonly token = environment.googlemap.apiKey;
+
+  findLocation: boolean = true;
 
   weather;
   weatherForecast;
@@ -35,7 +38,14 @@ export class MeteoComponent implements OnInit, AfterViewInit, OnDestroy {
   constructor(private _meteoService: MeteoService, private _timeZoneServie: TimeZoneService, private sanitizer: DomSanitizer) { }
 
   ngOnInit() {
-    this.getWeatherCity('Montpellier');
+
+    if (this.findLocation) {
+      this.getMeteo()
+    }
+    else {
+      this.getWeatherCity('Montpellier');
+    }
+
   }
 
   ngAfterViewInit() {
@@ -61,7 +71,8 @@ export class MeteoComponent implements OnInit, AfterViewInit, OnDestroy {
     this._meteoService.searchCityForecast(city).subscribe(
       res => {
         this.weatherForecast = res;
-        console.log(res)
+         console.log(res);
+        this.setData(res);
       }
     )
   }
@@ -80,10 +91,15 @@ export class MeteoComponent implements OnInit, AfterViewInit, OnDestroy {
 
   getMeteo(): void {
     this._meteoService.getCoords()
-      .subscribe(res => this.coords = res.json());
-    // console.log(this.coords)
-    // this.meteoService.searchLatLong(this.coords.lat, this.coords.long)
-    // .subscribe(res => this.meteo = res);
+      .subscribe(res => {
+         console.log(res)
+        // this.getWeatherCity(res.city)
+        this.getWeatherForecastCity(res.city)
+        
+      } 
+    )
+    // this._meteoService.searchLatLong(this.coords.lat, this.coords.long)
+    // .subscribe(res => this.setData(res));
   }
 
   searchCity() {
@@ -91,7 +107,8 @@ export class MeteoComponent implements OnInit, AfterViewInit, OnDestroy {
       this.city = false;
       this.error = false;
       this.timeZone = null;
-      this._meteoService.searchCity(this.cityName)
+      // this._meteoService.searchCity(this.cityName)
+      this._meteoService.searchCityForecast(this.cityName)
         .subscribe(
         (res) => {
           this.setData(res);
@@ -113,20 +130,32 @@ export class MeteoComponent implements OnInit, AfterViewInit, OnDestroy {
   }
 
   setData(res){
-    this.weather = res;
-    console.log(res);
-    this.getTimeZone(res.coord.lat, res.coord.lon);
-    this.map = this.sanitizer.bypassSecurityTrustResourceUrl(this.urlGoogleMap + res.coord.lat + ',' + res.coord.lon + '&key=' + this.token);
-    document.querySelector('body').style.backgroundImage = "url(" + this._meteoService.getBgMeteo(this.weather.weather[0].icon) + ")";
+    // this.weather = res;
+    this.weatherForecast = res
+    console.log(new Date())
+
+    this.getTimeZone(res.city.coord.lat, res.city.coord.lon);
+    this.map = this.sanitizer.bypassSecurityTrustResourceUrl(this.urlGoogleMap + res.lat + ',' + res.lon + '&key=' + this.token);
+    document.querySelector('body').style.backgroundImage = "url(" + this._meteoService.getBgMeteo(this.weatherForecast.list[0].weather[0].icon) + ")";
     document.querySelector('body').style.backgroundRepeat ="no-repeat";
     document.querySelector('body').style.backgroundSize = "cover";
   }
 
+  today(string) {
+    let now = new Date();
+    let date = new Date(string);
+    console.log(now.getDay());
+    console.log(date.getDay())
+    if (date.getDay() == now.getDay()) {
+      return true;
+    }
+  }
+
   forecastToggle() {
     this.forecast = !this.forecast;
-    if (this.forecast) {
-      this.getWeatherForecastCity('Montpellier')
-    }
+    // if (this.forecast) {
+    //   this.getWeatherForecastCity('Montpellier')
+    // }
   }
 
   meteoDetailsToggle() {
